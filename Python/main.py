d9119532aa41bd03e1928e3119b9d94e8821ffa8
Assefa_Altaye <assekopsynwa@gmail.com>
2026-02-19 22:40:37 -0600

    python basics

---
+++ Python/main.py
@@ -0,0 +1,137 @@
+# sales_price=float(input("Enter sales price: "))
+# tax_rate=float(input("Enter sales tax rate "))
+# #calculate tax
+# sales_tax=sales_price*tax_rate/100
+# purchase_price= sales_price+ sales_tax
+# print("Sales price: " ,sales_price, "Tax Rate:", tax_rate, "Sales tax: ", sales_tax, "Purchase price: ", purchase_price)
+
+#print('Hook \'em horns!')
+
+
+# name=input("Enter your name: ")
+# print("Hello, ", name)
+
+# length=8
+# width=5
+# area=length*width
+# print("The area is: ", area)
+
+# num1_str=input("Enter first number ")
+# num1= int(num1_str)
+# num2_str=input("Enter second number ")
+# num2= int(num2_str)
+# total =num1+num2
+# print(total)
+
+# birth_year=int(input("Enter birth year: "))
+# age=2025-birth_year
+# print("Yoour age is", age)
+
+# total_sale=float(input("Enter the projected amount of total sale: "))
+# annual_profit=total_sale*23/100
+# print("Total Profit is: ", annual_profit)
+
+
+# miles=float(input("Enter miles: "))
+# gallon=float(input("Enter gallons used: "))
+# print(miles/gallon)
+
+
+# price=float(input("Enter price per pen: "))
+# pens_bought=float(input("Enter number of pens: "))
+# money=float(input("Enter money: "))
+# remaining=money-(price*pens_bought)
+# print(remaining)
+
+
+# speed=35
+# rain=False
+# if rain:
+#     speed-10
+# if rain==False:
+#     speed=speed+5
+# if speed>35:
+#     speed= speed-2
+# print(speed)
+
+# num=int(input("Enter a number: "))
+# if num %2 ==0:
+#     print("Even")
+# else:
+#     print("Odd")
+
+
+# y=int(input("Enter the value of y:"))
+# if y==20:
+#     x=0
+# else:
+#     x=10
+# print(x)
+
+
+# age =22
+# if age >=18:
+#     if age <21:
+#         print("Young Adult")
+#     else:
+#         print("Adult")
+# else:
+#     print("Minor")
+
+# a=int(input("Enter the cvalue of a:"))
+# b=int(input("Enter the cvalue of b:"))
+# c= (a**2 + b**2)**0.5
+# print(c)
+
+# x=5.0
+# if x<5:
+#     x=3*x
+# if x/2==1:
+#     x=x/2
+# print (2*x +1 )
+
+# total=0
+# num=int(input("Enter a number (0 to stop):"))
+# while num !=0:
+#     total =total+num
+#     num=int(input("Enter a number (0 to stop):"))
+# print("Total =", total)
+
+
+# counter_reset=0
+# count=int(input("How many times to print "))
+
+# while counter_reset<count:
+#     print("Hello World!")
+#     counter_reset+=1    
+
+# count=1
+# while count<=10:
+#     count =count*2
+#     print(count)
+
+# n=1
+# while n<10:
+#     print("hello")
+#     n=n+2
+#     # print(n)
+
+# a=10
+# b=10
+# c=-10
+# if a>0 and b>0:
+#     print("The numbers are greater than zero")
+# if a>0 and b>0 and c>0:
+#     print("All three numbers are greater than zero")
+# else:
+#     print("Atleast one number is greater than zero")
+
+# print("Enter your name "
+#       "and your last name")
+print(f"{1234 * 10: ,d}")
+
+print(f"{1234 / 10: .2f}")
+
+print(f"{123467: ,.2f}")
+
+print(f"{.75: .0%}")
